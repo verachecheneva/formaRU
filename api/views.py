df043b85a438381c790f9ae6dbc7875a66086fd7
verachecheneva <74712988+verachecheneva@users.noreply.github.com>
2021-03-28 12:12:31 +0300

    1

--- api/views.py
+++ api/views.py
@@ -1,66 +1,64 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions, mixins, filters
+from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin
+
 
 from content.models import Application, Project, Tag, Questions, CompanyData, ImageProject
 from .serializers import ApplicationSerializer, ProjectReadSerializer, TagSerializer, QuestionsSerializer, \
     CompanyDataSerializer, ImageSerializer
 
 
-class ApplicationViewSet(viewsets.ModelViewSet):
+class ModelMixinSet(ListModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+class ApplicationMixinSet(CreateModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+class ApplicationViewSet(ApplicationMixinSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
     filter_backends = [DjangoFilterBackend]
     search_fields = ['name', 'email', 'phone_number']
 
-    def get_permissions(self):
-        if self.action == 'create':
-            permission_classes = [AllowAny]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
 
-
-class TagViewSet(viewsets.ModelViewSet):
+class TagViewSet(ModelMixinSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     filter_backends = [DjangoFilterBackend]
     search_fields = ['name', 'slug']
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class ProjectViewSet(viewsets.ModelViewSet):
+class ProjectViewSet(ModelMixinSet):
     queryset = Project.objects.order_by('num')
     serializer_class = ProjectReadSerializer
     filter_backends = [DjangoFilterBackend]
     search_fields = ['title', 'description', 'customer', 'num']
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class ImageViewSet(viewsets.ModelViewSet):
+class ImageViewSet(ModelMixinSet):
     queryset = ImageProject.objects.all()
     serializer_class = ImageSerializer
     filter_backends = [DjangoFilterBackend]
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         project = get_object_or_404(Project, pk=self.kwargs.get('project_id'))
         return ImageProject.objects.filter(project=project)
 
 
-class QuestionsViewSet(viewsets.ModelViewSet):
+class QuestionsViewSet(ModelMixinSet):
     queryset = Questions.objects.order_by('num')
     serializer_class = QuestionsSerializer
     filter_backends = [DjangoFilterBackend]
     search_fields = ['num', 'question', 'answer']
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class CompanyDataViewSet(viewsets.ModelViewSet):
+class CompanyDataViewSet(ModelMixinSet):
     queryset = CompanyData.objects.all()
     serializer_class = CompanyDataSerializer
     filter_backends = [DjangoFilterBackend]
     search_fields = ['email', 'phone_number']
-    permission_classes = [IsAuthenticatedOrReadOnly]
